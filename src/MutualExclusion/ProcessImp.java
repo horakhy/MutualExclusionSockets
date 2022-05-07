e667616782be23edefbd28c303b0f428d3422061
horakhy <leonidas51000@hotmail.com>
2022-05-07 13:03:11 -0300

    feat: umas ideias ae

--- src/MutualExclusion/ProcessImp.java
+++ src/MutualExclusion/ProcessImp.java
@@ -11,28 +11,32 @@ public class ProcessImp extends Thread {
   private final String IP = "230.0.0.0";
   private final int PORT = 4446;
   private final String MENSAGEM_ENTRADA = "ENTRADA";
+  private final String MENSAGEM_SOLICITACAO_SC = "SOLICITA SC";
+  private final String MENSAGEM_AUTORIZACAO_ACESSO_SC = "AUTORIZACAO ACESSO SC";
 
   private long id = (UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE) % 1000000;;
   protected MulticastSocket socket = null;
   protected byte[] buf = new byte[256];
   private ArrayList<ProcessImp> processosNaFila;
-  private ArrayList<Long> processosQueJaAvisaramEntrada;
+  private ArrayList<String> processosQueJaAvisaramEntrada;
+  private boolean solicitouAcessoSC = false;
 
   private int numeroProcessosConectados = 0;
+  private int numeroProcessosQueAutorizaramAcessoSC = 0;
   private StatusType status;
 
   public ProcessImp() {
     this.id = (UUID.randomUUID().getLeastSignificantBits() & Long.MAX_VALUE) % 10000;
     this.processosNaFila = new ArrayList<ProcessImp>();
-    this.processosQueJaAvisaramEntrada = new ArrayList<Long>();
+    this.processosQueJaAvisaramEntrada = new ArrayList<String>();
     System.out.println("Processo " + this.id + " criado");
     this.status = StatusType.RELEASE;
   }
 
-  private void notificarOutrosProcessos() throws IOException {
-
+  private void notificarSolicitacaoOutrosProcessos() throws IOException {
+    this.solicitouAcessoSC = true;
     // make datagram packet
-    byte[] message = ("Multicasting...").getBytes();
+    byte[] message = (MENSAGEM_SOLICITACAO_SC + " " + this.id).getBytes();
     DatagramPacket packet = new DatagramPacket(message, message.length,
         InetAddress.getByName(IP), PORT);
     // send packet
@@ -43,7 +47,7 @@ public class ProcessImp extends Thread {
   private void notificarEntrada(long id) throws IOException {
 
     // make datagram packet
-    byte[] message = ("" + this.id).getBytes();
+    byte[] message = (MENSAGEM_ENTRADA + " " + this.id).getBytes();
     DatagramPacket packet = new DatagramPacket(message, message.length,
         InetAddress.getByName(IP), PORT);
     // send packet
@@ -55,13 +59,32 @@ public class ProcessImp extends Thread {
     System.out.println("Registrando interesse...");
     this.status = StatusType.WANTED;
     
-    notificarOutrosProcessos();
+    notificarSolicitacaoOutrosProcessos();
   }
 
-  private void atualizarParesConectados(String received) throws IOException{
-    if(received.equals("" + this.id) || processosQueJaAvisaramEntrada.contains(Long.parseLong(received))) return;
+  public void notificarAutorizacaoAcessoSC() throws IOException {
+    byte[] message = (MENSAGEM_AUTORIZACAO_ACESSO_SC).getBytes();
+    DatagramPacket packet = new DatagramPacket(message, message.length,
+        InetAddress.getByName(IP), PORT);
+    // send packet
+    this.socket.send(packet);
+  }
+
+  public void liberarSC() throws IOException {
+    System.out.println("Liberando SC...");
+    this.status = StatusType.RELEASE;
     
-    processosQueJaAvisaramEntrada.add(Long.parseLong(received));
+    // notificarLiberacaoOutrosProcessos();
+  }
+
+
+  private void atualizarParesConectados(String received) throws IOException{
+    // Verifica se não é o processo enviando para si mesmo 
+    // Ou se já não recebeu a mensagem de entrada desse processo antes
+    if(received.contains("" + this.id) || processosQueJaAvisaramEntrada.contains(received)) return;
+    
+    processosQueJaAvisaramEntrada.add(received);
+    processosQueJaAvisaramEntrada.forEach(processo -> { System.out.println(processo);});
     notificarEntrada(this.id);
     numeroProcessosConectados++;
   }
@@ -87,7 +110,28 @@ public class ProcessImp extends Thread {
         System.out.println("Processo " + this.id + " recebeu: " + received);
         System.out.println("Numero de processos conectados: " + numeroProcessosConectados);
         System.out.println();
-        atualizarParesConectados(received);
+        
+        // Verifica se é uma mensagem de entrada
+        if(received.contains(MENSAGEM_ENTRADA)){
+          atualizarParesConectados(received);
+        }
+
+        if(received.contains(MENSAGEM_SOLICITACAO_SC)){
+          if(this.status == StatusType.HELD){
+            return;
+          }
+          notificarAutorizacaoAcessoSC();
+        }
+
+        if(received.equals(MENSAGEM_AUTORIZACAO_ACESSO_SC) && this.status == StatusType.WANTED && this.solicitouAcessoSC){
+          System.out.println("ENTREI AWUIII");
+          numeroProcessosQueAutorizaramAcessoSC++;
+          if(numeroProcessosQueAutorizaramAcessoSC <= numeroProcessosConectados - 1){
+            this.status = StatusType.HELD;
+            System.out.println("ESTOU COM A SC");
+          }
+        }
+
         // if(received.equals(MENSAGEM_ENTRADA + " " + this.id)) return;
         //   notificarEntrada(this.id);
         //   numeroProcessosConectados++;
